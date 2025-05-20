0f23e9eb1123bb9f719ae3d1b10862be483c05de
Erik <erik.fernandes87@gmail.com>
2025-05-19 22:43:40 -0300

    require pedidoController

--- src/interfaces/http/routes/PedidoRoutes.js
+++ src/interfaces/http/routes/PedidoRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const verifyToken = require('../middlewares/verifyToken');
-const pedidoController = require('/../../application/controllers/pedidoController');
+const pedidoController = require('../../../application/controllers/pedidoController');
 
 router.use(verifyToken);
 
